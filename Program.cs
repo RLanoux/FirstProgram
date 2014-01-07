4cde62c501c806c2d8712f819e78db1107b14c3c
Raymond Lanoux <raymondlanoux@gmail.com>
2014-01-06 18:32:19 -0600

    initial add

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace First_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Programming!");
+            Console.ReadLine();
+        }
+    }
+}
